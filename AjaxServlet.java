61ac83ce4dbdb7b9a8cacfa21a728b7a70c0786a
zhaoguanpeng <50667721+zhaoguanpeng@users.noreply.github.com>
2020-11-03 13:02:08 +0800

    Add files via upload

---
+++ AjaxServlet.java
@@ -0,0 +1,25 @@
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+@WebServlet("/ajaxServlet")
+public class AjaxServlet extends HttpServlet {
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        String username=req.getParameter("username");
+//        try {
+////            Thread.sleep(5000);
+////        } catch (InterruptedException e) {
+////            e.printStackTrace();
+////        }
+        System.out.println(username);
+        resp.getWriter().write("hello"+username);
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        this.doPost(req,resp);
+    }
+}
